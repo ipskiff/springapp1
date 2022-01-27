be6b6fd5a9eb71a670ee1e692859b7da78feeeb0
ipskiff <ipskiff007@gmail.com>
2022-01-27 02:47:59 +0200

    Initial commit

---
+++ src/main/java/ru/satizhanov/myspringapp/TestBean.java
@@ -0,0 +1,19 @@
+package ru.satizhanov.myspringapp;
+
+public class TestBean {
+    private String name;
+
+    public TestBean(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
